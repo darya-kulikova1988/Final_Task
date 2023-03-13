317dc458e3159f00977baeb8f873854c843eecbe
Darya <darya.kulikova88@yandex.ru>
2023-03-13 15:54:44 +0300

    число n+nn+nnn

---
+++ seminars/extra1.py
@@ -0,0 +1,17 @@
+# При заданном целом числе n посчитайте n + nn + nnn
+n = input('введите n: ')
+a = int(n)+int(n*2)+int(n*3)
+print(a)
+# c = n*2
+# print(c)
+# c = int(c)
+# b = n+n+n
+# print(b)
+# b = int(b)
+# n = int(n)
+# a = n+c+b
+# print(a)
+
+n = int(input('введите n: '))
+a = n+n*10+n+n*100+n*10+n
+print(a)
